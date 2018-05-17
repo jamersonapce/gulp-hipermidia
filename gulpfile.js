ea4e5555b63ccea0dbe5c7b5df176d07c0757a11
jamersonapce <jamerson.silva@acad.ifma.edu.br>
2018-05-17 17:10:25 -0300

    Implementação tarefa clean

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
+const clean = require('gulp-clean');
 
+
+gulp.task('clean', function() {
+    return gulp.src('dist')
+        .pipe(clean());
+});
 
 gulp.task('build-img', function() {
     gulp.src('src/img/**/*')
