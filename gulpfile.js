0e35238d5a9736ca2428b364c039b2c7a9d2d041
jamersonapce <jamerson.silva@acad.ifma.edu.br>
2018-05-17 16:51:48 -0300

    minificando as imagens

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,9 @@
-var gulp = require('gulp');
+const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+
+
+gulp.task('build-img', function() {
+    gulp.src('src/img/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/img'));
+});
